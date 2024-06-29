7889f0d3118da0f4a220799e5f147304d3affb0c
abhi111456 <abhishek.21jics039@jietjodhpur.ac.in>
2024-06-29 13:40:33 +0530

    a

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 import pickle
-import os
 
 app = Flask(__name__)
 
@@ -25,10 +24,6 @@ loaded_final_rf_model = saved_data['final_rf_model']
 loaded_symptom_index = saved_data['symptom_index']
 loaded_predictions_classes = saved_data['predictions_classes']
 
-current_dir = os.path.abspath('.')
-
-# Initialize the Flask app with the current directory as the template folder
-app = Flask(__name__, template_folder=current_dir)
 @app.route('/')
 def index():
     return render_template("index.html")
